99569fb6aa86eba20c0ef0526b6b2b1d22546ffa
John Marín <jmarinsocha@hotmail.com>
2024-04-18 21:50:38 -0500

    Ajuste cálculo y opciones

--- src/PrincipalConversorMonedas.java
+++ src/PrincipalConversorMonedas.java
@@ -1,7 +1,6 @@
 import com.jems.challenge.api.APIExchange;
 import com.jems.challenge.api.TasaDeCambio;
 import com.jems.challenge.calculos.ConversorMonedas;
-import static com.jems.challenge.utilidades.FormatearMoneda.formatearMoneda;
 import com.jems.challenge.utilidades.Sleep;
 
 import java.util.Map;
@@ -15,7 +14,7 @@ public class PrincipalConversorMonedas {
         String opciones = """
                 ***************************************
                 Seleccione una opción:
-                [1] Ingresar valor
+                [1] Calcular otro valor
                 [2] Seleccionar otras monedas
                 [9] Salir
                 [0] Ayuda
@@ -23,30 +22,41 @@ public class PrincipalConversorMonedas {
                 """;
 
         String ayuda = """
-                            Por ejemplo: ARS | BRL | COP | MXN | USD | ...otras
                             Puede convertir cualquier moneda del mundo.
+                            Algunos ejemplos:
+                            ----------------------------------------------------------------
+                            | País                        | Divisa                | Código |
+                            ----------------------------------------------------------------
+                            | ESTADOS UNIDOS DE AMÉRICA   | Dólar estadounidense  |  USD   |
+                            ----------------------------------------------------------------
+                            | UNIÓN EUROPEA               | Euro                  |  EUR   |
+                            ----------------------------------------------------------------
+                            | COLOMBIA                    | Peso colombiano       |  COP   |
+                            ----------------------------------------------------------------
                             """;
 
         System.out.println("***************************************");
         System.out.println("Bienvenid@ al Conversor de Monedas");
         System.out.println("***************************************");
-        System.out.println("\n>>> Por favor ingrese los códigos de las monedas que desea cambiar.");
+        System.out.println("\n>>> Ingrese los códigos de las monedas que desea cambiar.");
         System.out.println(ayuda);
         Scanner sc = new Scanner(System.in);
         System.out.println("--> Primero ingrese la moneda para tomar como base: ");
-        String cambiarDe = sc.nextLine().toUpperCase();
+        String monedaBase = sc.nextLine().toUpperCase();
         System.out.println("--> Ahora ingrese la segunda moneda (a la que desea cambiar): ");
-        String cambiarA = sc.nextLine().toUpperCase();
+        String monedaCambio = sc.nextLine().toUpperCase();
 
         ConversorMonedas cm = new ConversorMonedas();
         APIExchange consulta = new APIExchange();
-        TasaDeCambio tc = consulta.buscaTasaCambio(cambiarDe);
+        TasaDeCambio tc = consulta.buscaTasaCambio(monedaBase);
         Map<String, Double> mapTasaDeCambio = (Map<String, Double>) tc.conversion_rates();
-        Double tasaCambio = mapTasaDeCambio.get(cambiarA);
+        Double tasaCambio = mapTasaDeCambio.get(monedaCambio);
+
+        cm.cambiarMoneda(monedaBase, monedaCambio, tasaCambio);
 
         while (opcion != 9){
             System.out.println("***************************************");
-            System.out.format("Cambiar de %s a %s", cambiarDe, cambiarA);
+            System.out.format("Cambiar de %s a %s", monedaBase, monedaCambio);
             System.out.println("\n" + opciones);
 
             opcion = sc.nextInt();
@@ -58,22 +68,20 @@ public class PrincipalConversorMonedas {
 
             switch (opcion){
                 case 1:
-                    System.out.format("¿Cuál es el valor en %s que desea cambiar a %s?\n", cambiarDe, cambiarA);
-                    Scanner scValorACambiar = new Scanner(System.in);
-                    Double valorACambiar = scValorACambiar.nextDouble();
-                    String valor = cm.getValorCambio(tasaCambio, valorACambiar);
-                    String fValorACambiar = formatearMoneda(valorACambiar);
-                    System.out.format("%s %s es igual a %s %s \n", fValorACambiar, cambiarDe, valor, cambiarA);
-                    System.out.format("El valor de cambio de $ 1 %s es %s %s\n", cambiarDe, formatearMoneda(tasaCambio), cambiarA);
+                    cm.cambiarMoneda(monedaBase, monedaCambio, tasaCambio);
                     Sleep.sleep();
                     break;
 
                 case 2:
                     System.out.println("--> Primero ingrese la moneda para tomar como base: ");
                     Scanner scNuevaMoneda = new Scanner(System.in);
-                    cambiarDe = scNuevaMoneda.nextLine().toUpperCase();
+                    monedaBase = scNuevaMoneda.nextLine().toUpperCase();
                     System.out.println("--> Ahora ingrese la segunda moneda (a la que desea cambiar): ");
-                    cambiarA = scNuevaMoneda.nextLine().toUpperCase();
+                    monedaCambio = scNuevaMoneda.nextLine().toUpperCase();
+                    tc = consulta.buscaTasaCambio(monedaBase);
+                    mapTasaDeCambio = (Map<String, Double>) tc.conversion_rates();
+                    tasaCambio = mapTasaDeCambio.get(monedaCambio);
+                    cm.cambiarMoneda(monedaBase, monedaCambio, tasaCambio);
                     Sleep.sleep();
                     break;
 
@@ -84,7 +92,9 @@ public class PrincipalConversorMonedas {
 
                 case 0:
                     System.out.println("""
-                            Un texto va acá........
+                            Moneda base: Es la moneda que desea cambiar a otra.
+                            Moneda cambio: Es la moneda a la que desea convertir el valor.
+                            Puede ingresar en la aplicación los códigos de divisa que correspondan a cada país.
                             """);
                     Sleep.sleep();
                     break;
--- src/com/jems/challenge/calculos/ConversorMonedas.java
+++ src/com/jems/challenge/calculos/ConversorMonedas.java
@@ -1,10 +1,24 @@
 package com.jems.challenge.calculos;
 
+import java.util.Scanner;
+
 import static com.jems.challenge.utilidades.FormatearMoneda.formatearMoneda;
 
 public class ConversorMonedas {
 
     public String getValorCambio(Double tasaCambio, Double valorACambiar) {
         return formatearMoneda(tasaCambio * valorACambiar);
+    }
+
+    public void cambiarMoneda(String monedaBase, String monedaCambio, Double tasaCambio){
+        System.out.format("¿Cuál es el valor en %s que desea cambiar a %s?\n", monedaBase, monedaCambio);
+        Scanner scValorACambiar = new Scanner(System.in);
+        Double valorACambiar = scValorACambiar.nextDouble();
+        String valor = getValorCambio(tasaCambio, valorACambiar);
+        String fValorACambiar = formatearMoneda(valorACambiar);
+        System.out.println("-------------------------------------------------");
+        System.out.format("%s %s es igual a %s %s \n", fValorACambiar, monedaBase, valor, monedaCambio);
+        System.out.format("El valor de cambio de $ 1 %s es %s %s\n", monedaBase, formatearMoneda(tasaCambio), monedaCambio);
+        System.out.println("-------------------------------------------------");
     }
 }
